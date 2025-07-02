dbc8ba037d9467a74d98db6d8638a95d0340b99f
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-02 10:02:33 +0300

    movies

--- src/lib/formatDuration.js
+++ src/lib/formatDuration.js
@@ -2,21 +2,21 @@ export function formatDuration(minutes = 100) {
     const min = minutes % 60;
     const h = (minutes - min) / 60;
 
-    let minText = min + ' minute ';
+    let minText = '';
     if (min > 0) {
-        minText += 's';
+        minText += min + ' minute';
     }
     if (min > 1) {
         minText += 's';
     }
-    
-    let hourText = '';
+
+    let hourText = ''
     if (h > 0) {
-        hourText = h + ' hour ';
+        hourText += h + ' hour';
     }
     if (h > 1) {
         hourText += 's';
     }
 
-    return hourText + (hourText && minText ? '' : '') + minText;
+    return hourText + (hourText && minText ? ' ' : '') + minText;
 }
