3112b8c4d15fe8867c2b1eb39fd2b117f9d9fc4c
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-02 09:48:07 +0300

    movies filter

--- src/data/moviesData.js
+++ src/data/moviesData.js
@@ -1,5 +1,5 @@
 export const moviesData = [ {
     title: 'Matrix',
     url:'',
-    description:
+    description:'',
 } ];
---
+++ src/ui/forms/moviesFilterForm.js
@@ -0,0 +1,4 @@
+export function moviesFilterForm() {
+    return `
+    <form></form>`
+}
