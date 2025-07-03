592315cca020f501cb441b108495599d682b5fa8
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-03 10:43:30 +0300

    DB

--- src/db.js
+++ src/db.js
@@ -1,1 +1,9 @@
+import mysql from 'mysql2/promise';
 
+export const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    database: '55gr_movies',
+    user: 'root',
+    password: '', 
+});
--- src/env.js
+++ src/env.js
@@ -19,6 +19,8 @@ dotenv.config({
 
 export const PORT = +process.env.PORT ?? 5517;
 export const TITLE = process.env.TITLE ?? 'Project title';
+export const DB_HOST = process.env.DB_HOST ?? 'localhost';
+export const DB_PORT = +process.env.DB_PORT ?? 3306;
 export const DB_DATABASE = process.env.DB_DATABASE ?? 'test_db'; 
 export const DB_USER = process.env.DB_USER ?? 'test_user'; 
 export const DB_PASSWORD = process.env.DB_PASSWORD  ?? 'test_password';
