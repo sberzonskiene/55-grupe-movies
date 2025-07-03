afc77ded79789994db281f3a41dbed731266d78b
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-03 10:07:58 +0300

    validation

--- src/api/public/postRegister.js
+++ src/api/public/postRegister.js
@@ -1,6 +1,22 @@
+import { IsValid } from "../../lib/isValid.js";
+
 export function postRegister(req, res) {
+    const [err, msg] = IsValid.fields(req.body, {
+        username: 'username',
+        email: 'email',
+        password: 'password',
+        tos: 'tos',
+    });
+
+    if (err) {
+        return res.json({
+            status: 'error',
+            msg: msg,
+        });
+    }
+
     console.log(req.body);
-    
+
     return res.json({
         status: 'success',
         msg: 'Sekminga registracija',
--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -9,18 +9,9 @@ export const publicRouter = express.Router();
 
 publicRouter.get('/', (req, res) => res.send(new PageHome().render()));
 
-publicRouter.get('/movies', (req, res) => {
-    return res.send(new PageMovies().render());
-});
+publicRouter.get('/movies', (req, res) => res.send(new PageMovies().render()));
 
-publicRouter.get('/categories', (req, res) => {
-    return res.send(new PageCategories().render());
-});
+publicRouter.get('/categories', (req, res) => res.send(new PageCategories().render()));
 
-publicRouter.get('/login', (req, res) => {
-    return res.send(new PageLogin().render());
-});
-
-publicRouter.get('/register', (req, res) => {
-    return res.send(new PageRegister().render());
-});
+publicRouter.get('/login', (req, res) => res.send(new PageLogin().render()));
+publicRouter.get('/register', (req, res) => res.send(new PageRegister().render()));
