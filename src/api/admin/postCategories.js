5aca431cf701ac10e3e4c63291883c7c54f57e5d
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-14 09:41:07 +0300

    postCategories

--- src/api/admin/postCategories.js
+++ src/api/admin/postCategories.js
@@ -19,8 +19,45 @@ export async function postCategories(req, res) {
 
     const { title, url, status, description } = req.body;
 
-    // SELECT
-    // INSERT
+    try {
+        const sql = `SELECT * FROM categories WHERE title = ? OR url_slug = ?;`;
+        const [response] = await connection.execute(sql, [title, url]);
+
+        if (response.length > 0) {
+            return res.status(400).json({
+                status: 'error',
+                msg: 'Tokia kategorija jau egzistuoja',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 'error',
+            msg: 'Serverio klaida',
+        });
+    }
+
+    try {
+        const sql = `
+            INSERT INTO categories (title, url_slug, status_id, description)
+            VALUES (?, ?, 
+                (SELECT id FROM category_status WHERE name = ?),
+                ?);`;
+        const [response] = await connection.execute(sql, [title, url, status, description]);
+
+        if (response.affectedRows !== 1) {
+            return res.status(500).json({
+                status: 'error',
+                msg: 'Serverio klaida',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 'error',
+            msg: 'Serverio klaida',
+        });
+    }
 
     return res.status(201).json({
         status: 'success',
